112bd725a8827eddd25cc4c3552a9d3432b1660c
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2021-03-08 20:42:56 -0800

    Change load_settings into settings file

--- load_settings.py
+++
@@ -1,9 +0,0 @@
-"""
-Load Settings YAML files for 3dprinter_start_experiment
-
-Purpose: Replaces Common Python file, allows for future GUI usage where the YAML files can be edited and reloaded
-"""
-
-# Import libraries, yaml
-
-# Load YAML file, store into constants
---
+++ settings.py
@@ -0,0 +1,35 @@
+"""
+Load Settings YAML files for 3dprinter_start_experiment
+
+Purpose: Replaces Common Python file, allows for future GUI usage where the YAML files can be edited and reloaded
+"""
+
+# Import libraries, yaml
+import yaml
+# Create Constants Variables, like in Common file
+
+# Load YAML files, store into constants
+# Load YAML Settings
+# Monoprice Maker Select 3D Printer V2, Lab 3D Printer
+DEVICE_PATH = '/dev/ttyUSB0'
+BAUDRATE = 115200     # 115200: for Marlin Firmware
+TIMEOUT_TIME = 1      # Wait 1 second
+REBOOT_WAIT_TIME = 5  # 5 seconds
+
+# Load GCODE Strings, or put them here temporarily
+# GCode Strings
+HOME = "G28"
+ABSOLUTE_POS = "G90"
+RELATIVE_POS = "G91"
+
+# Load YAML Settings
+with open("connection_settings.yaml") as file:
+    # The fullloader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    connection_settings_dict = yaml.load(file, Loader=yaml.FullLoader)
+    # How to access dict items
+    # print(connection_settings_dict["cell_sensor"]["monoprice"]["device_path"])
+
+# User Defined function that can change Constants by having user load up a YAML file and choosing a different 3D printer
+# TODO: Later feature
+# TODO: Research project structure for GUI and Settings file
