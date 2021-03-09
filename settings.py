044156cf9f27e120c8df3061333739305b715fbc
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2021-03-08 20:50:14 -0800

    Created Settings Working Settings file, works like Common file now

--- settings.py
+++ settings.py
@@ -10,6 +10,8 @@ import yaml
 
 # Load YAML files, store into constants
 # Load YAML Settings
+
+# Placeholder Constants
 # Monoprice Maker Select 3D Printer V2, Lab 3D Printer
 DEVICE_PATH = '/dev/ttyUSB0'
 BAUDRATE = 115200     # 115200: for Marlin Firmware
@@ -22,6 +24,10 @@ HOME = "G28"
 ABSOLUTE_POS = "G90"
 RELATIVE_POS = "G91"
 
+# Which Project? Will influence which settings are loaded
+# PROJECT = "mht"
+PROJECT = "cell_sensor"
+
 # Load YAML Settings
 with open("connection_settings.yaml") as file:
     # The fullloader parameter handles the conversion from YAML
@@ -29,7 +35,12 @@ with open("connection_settings.yaml") as file:
     connection_settings_dict = yaml.load(file, Loader=yaml.FullLoader)
     # How to access dict items
     # print(connection_settings_dict["cell_sensor"]["monoprice"]["device_path"])
+    DEVICE_PATH = connection_settings_dict[PROJECT]["monoprice"]["device_path"]
+    BAUDRATE = connection_settings_dict[PROJECT]["monoprice"]["baudrate"]
+    TIMEOUT_TIME = connection_settings_dict[PROJECT]["monoprice"]["timeout_time"]
+    REBOOT_WAIT_TIME = connection_settings_dict[PROJECT]["monoprice"]["reboot_wait_time"]
 
 # User Defined function that can change Constants by having user load up a YAML file and choosing a different 3D printer
 # TODO: Later feature
 # TODO: Research project structure for GUI and Settings file
+
