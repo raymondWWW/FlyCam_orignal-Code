3b1dd3210a5d967973140cea0550c51df46cf25e
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2021-03-31 00:37:38 -0700

    Added in commented links for research

--- testing/m114_string_parse.py
+++ testing/m114_string_parse.py
@@ -13,6 +13,7 @@ from parse import *
 # https://marlinfw.org/docs/gcode/M114.html
 
 sample = "X:1.45Y:2.67Z:100.09E:3.00 Count X: 4.00Y:5.00Z:102.00\nok"
+sample2 = "wait\nwait\nok\nX:0.00 Y:0.00 Z0.000 E:0.0000"
 # sample = "echo: Position"
 # sample = sample.splitlines()
 parsed_str = sample[0].split("Count")
@@ -45,6 +46,8 @@ Check if current position is destination position, if true take photo/video, els
 
 Send M114
 Get receiving bytes
+https://pythonhosted.org/pyserial/pyserial_api.html#serial.Serial.in_waiting
+https://stackoverflow.com/questions/38645060/what-is-the-equivalent-of-serial-available-in-pyserial
 
 if greater than 50 and less than 100, probably destination and current position information
 Start parsing algorithm
@@ -55,14 +58,27 @@ Start parsing algorithm
 
 parse_position_information():
 Assumes string is in this kind of format:
-"X:1.45Y:2.67Z:100.09E:3.00 Count X: 4.00Y:5.00Z:102.00\nok"
+"X:1.45Y:2.67Z:100.09E:3.00 Count X: 4.00Yt:5.00Z:102.00\nok"
+(Destination, Current)
+or
+
+"wait
+wait
+ok
+X:1.45Y:2.67Z:100.09E:3.00
+"
 
 Initialize destination_position_dictionary and current_position_dictionary with keys: x, y, z
  use parse library findall() function to look for findall("X:{x:.2f}Y:{y:.2f}Z:{z:.2f}", input)
  store into dictionaries using for loop to iterate through results of findall()
  return dictionaries
 
-
+Update:
+- Include different sample inputs
+- Search for only the X, Y, and Z
+  - If none, keep waiting or request M114
+  - If one, use that as current location
+  - If two or more, use last one
  
 """
 
