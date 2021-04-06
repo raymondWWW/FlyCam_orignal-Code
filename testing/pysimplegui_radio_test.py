a1b6d0da6f56a217baf8263e49b33337f8a6dd34
Johnny D (SFSU) <10577482+JohnDoRight@users.noreply.github.com>
2021-04-06 00:55:09 -0700

    PySimpleGUI Radio Test Successful

--- testing/pysimplegui_radio_test.py
+++ testing/pysimplegui_radio_test.py
@@ -1,4 +1,28 @@
-# PySimpleGui Radio Testing
+# PySimpleGUI Radio Testing
 
-import pysimplegui as sg
+import PySimpleGUI as sg
 
+sg.theme("DarkBlue15")
+
+layout = [  [sg.Button("Hello World", size=(20, 4))],
+            [sg.Checkbox("Print On:", default=True, key="-IN-")],
+            [sg.Radio("Permission Granted", "RADIO1", default=False, key="-IN2-")],
+            [sg.Radio("Permission not Granted", "RADIO1", default=True)]]
+
+# Setting Window
+
+window = sg.Window("Push my Buttons", layout, size=(300, 300))
+
+
+# Show the Application
+
+while True:
+    event, values = window.read()
+    print(values)
+    if event == sg.WIN_CLOSED or event=="Exit":
+        break
+    elif values["-IN-"] == True and values["-IN2-"] == True:
+        print("Hello, World!")
+
+
+window.close()
