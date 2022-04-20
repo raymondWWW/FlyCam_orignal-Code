a4f0fdd4aef0d2c817b1ed85bd7d3c8676ba7a0d
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-04-20 12:58:15 -0700

    Create cam_z_stack.py
    Created cam_z_stack.py. Put in pseudocode for how algorithm will work.

---
+++ testing/cam_z_stack_test.py
@@ -0,0 +1,34 @@
+"""
+Test Code for Z-Stack Capturing using a Raspberry Pi, Camera, and 3D printer.
+
+Test code will only be algorithm that does not include camera and 3D printer.
+
+Algorithm:
+-Gets Current Location, assumes sending out of dictionary with location X, Y, and Z (gets z_current)
+   - For actual, will have to be called twice to remove old location?
+-Runs for loop with z_start, z_end and z_increment
+   -z_current will be used in saving image file. Filename format: image_[z].jpg
+   -For loop converts z_current float to GCODE string (if I have code that does this, use that)
+-Saves images to a unique folder that is created (use my modification of Adrian's code for this)
+-GUI:
+  -allows user to set z_start, z_end and z_increment
+  -Has button to start z_stack creation
+  -is a different tab
+  -Optional: Displays current location and allows user to input x/y/z for absolute input
+
+Optional:
+-Create function to convert float x/y/z to GCODE, and send GCODE.
+"""
+
+# Define function that runs GUI for Z Stack Creation
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
