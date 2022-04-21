a0486499060a40bdd779aeba41ef50e2b87a518d
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-04-20 22:03:09 -0700

    Create save_location_test_code.py
    Init commit, also put in sample PySimpleGUI code.

---
+++ testing/save_location_test_code.py
@@ -0,0 +1,15 @@
+"""
+Test Code to Save 3D printer Get Current Location to a CSV File
+
+Test input data is a dictionary containing X/Y/Z float values.
+Optional: Have a list of dictionaries to simulate get multiple current locations.
+
+Version 1: Push a button to save to an existing CSV file
+
+Version 2: Display CSV file that allows you to add and delete rows.
+
+
+Code Sources:
+https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Table_CSV.py
+
+"""
