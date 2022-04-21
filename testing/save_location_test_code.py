d865f781f5b474505cfaf21ce604beec6fad04d2
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-04-21 12:11:36 -0700

    Update save_location_test_code
    Added in pseudocode.

--- testing/save_location_test_code.py
+++ testing/save_location_test_code.py
@@ -17,6 +17,27 @@ https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Table_S
 
 
 def main():
+
+    # Set up GUI Layout (A single button)
+
+    # Setup window
+
+    # Setup text file (hardcoded for now), save as CSV or use CSV library
+    # Probably make it a constant, or create on GUI start?
+
+    # Start forever while loop for GUI
+
+    #  Have if statement for closing GUI
+
+    #  If button pressed
+    #    Get current location (assume one for now)
+    #    Convert dictionary to a CSV row
+    #    Append to hardcorded CSV file
+    #      Bugs/ideas:
+    #       What about duplicates?
+    #       Are the rows numbered?
+    #       Should I add a text editor or row CSV deleter? What would the user expect for easy editing?
+    #       Or is this a "well plate profile"?
     pass
 
 
