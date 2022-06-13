5861f3373327381504a2a186b5fc0e1496b670eb
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-06-13 00:18:53 -0700

    Update well_loc_calc_test.py
    Turned row locations calculations into functions, is working.

--- testing/well_loc_calc_test.py
+++ testing/well_loc_calc_test.py
@@ -35,16 +35,107 @@ Ideas:
 """
 
 
-def main():
-    print("Main")
+def get_delta_dict(start_loc, end_loc):
+    """
+    Given two location dictionaries, get difference between them,
+    return delta (or difference) dictionary. Assumes both dictionaries have
+    the same keys.
 
+    :param start_loc: A dictionary, format: {"X": 0.00, "Y": 0.00, "Z": 0.00}
+    :param end_loc: A dictionary, format: {"X": 0.00, "Y": 0.00, "Z": 0.00}
+    :return: A dictionary, delta dictionary, format: {"X": 0.00, "Y": 0.00, "Z": 0.00}
+    """
+
+    # Init empty dictionary
+    delta_dict = {}
+
+    # Use for loop to go through both keys of both dictionaries.
+    for key in start_loc.keys():
+        # Uncomment to print out key to make sure loop is working
+        # print(key)
+
+        # Calculate difference (or delta) between each X, Y, and Z
+        delta_dict[key] = end_loc[key] - start_loc[key]
+
+    # Uncomment to get print out delta_dict
+    # print(f"delta_dict: {delta_dict}")
+    return delta_dict
+
+
+def get_row_locations(row_start, row_end, num_col):
+    """
+    Given the starting and ending row location, and number of columns,
+    calculate each well's location in a row.
+
+    TODO: Figure out if should return a dictionary using row/col as keys?
+
+    :param row_start: A dictionary, format: {"X": 0.00, "Y": 0.00, "Z": 0.00}
+    :param row_end: A dictionary, format: {"X": 0.00, "Y": 0.00, "Z": 0.00}
+    :param num_col: An int, the number of columns in well plate
+    :return: A list, list of well locations of a well
+    """
+
+    num_col_jump = num_col - 1
+
+    # Get Step Distance Values
+    # Maximum number of jumps to go from well 1 to last well.
+    # Example: If there are 5 wells, you start at well 1.
+    #          How many jumps does it take to go from well 1 to well 5?
+    #          Answer 4, or number of columns - 1.
+    # Used to calculate distance to jump to next well.
+    max_col_jump = num_col - 1
+
+    # Get Delta Dictionary,
+    # used for calculating stepsize (or getting distance to jump to next well).
+    delta_dict = get_delta_dict(row_start, row_end)
+
+    # Initialize empty well_loc_list, to be appended later
+    well_loc_list = []
+
+    # print(f"row_start: {row_start}")
+    # Calculate row well locations using number of columns
+    for col in range(num_col):
+
+        # Init empty dictionary well_loc
+        well_loc = {}
+        # Loop Version, go through each key (X, Y, or Z), calculate location
+        for key in row_start.keys():
+            # print(key)
+            # Get Starting Location, as X, Y, or Z (individually)
+            start = row_start[key]
+
+            #  Get stepsize (in mm) amount to jump to next well
+            step = delta_dict[key] / max_col_jump
+
+            # Calculate actual location, round to two decimal places
+            loc = round(start + (step * col), 2)
+
+            # Add that location (float value) to that specific key (X, Y, or Z)
+            well_loc[key] = loc
+
+        # Print out location, make sure it increments correctly.
+        # Prints out column number (index starts at 1 since physical well plates do that)
+        # print(f"col: {col+1}, {well_loc}")
+
+        # Append location to well_loc_list
+        well_loc_list.append(well_loc)
+
+    # Print out row_end location for comparison and debugging.
+    # Last calculated row location (actual) should match row_end (expected)
+    # print(f"row_end: {row_end}")
+
+    # Output those locations
+    # print(f"well_loc_list: {well_loc_list}")
+
+    return well_loc_list
+
+
+def main():
     # Location format: location_dictionary = {"X": 0.00, "Y": 0.00, "Z": 0.00}
 
     # Placeholder data, row 1 start/end, number of rows and columns
     num_row = 3
     num_col = 5
-    # Number of jumps to go from first well to last well, column-wise
-    num_col_jump = num_col - 1
 
     X = "X"; Y = "Y"; Z = "Z"
     key_list = [X, Y, Z]
@@ -53,61 +144,8 @@ def main():
     row_start = {"X": 1.00, "Y": 2.00, "Z": 3.00}
     row_end = {"X": 51.00, "Y": 3.00, "Z": 1.00}
 
-    # Calculate location of each well in row using input number of columns
-
-    # delta_x = abs(row_end[X] - row_start[X])
-    # delta_y = abs(row_end[Y] - row_start[Y])
-    # delta_z = abs(row_end[Z] - row_start[Z])
-    # print(delta_z)
-
-    delta_dict = {}
-    for key in row_start.keys():
-        # print(key)
-        # delta_dict[key] = abs(row_end[key] - row_start[key])
-        delta_dict[key] = row_end[key] - row_start[key]
-
-    delta_x = delta_dict[X]
-    print(f"delta_dict: {delta_dict}")
-    print(f"delta_x: {delta_x}")
-
-    # Difference between consecutive well centers, x direction
-    # well_length_x = round(delta_x / num_col, 2)
-    well_length_x = delta_x / num_col_jump
-    x_step = delta_dict[X] / num_col_jump
-    y_step = delta_dict[Y] / num_col_jump
-    z_step = delta_dict[Z] / num_col_jump
-
-    print(f"well_length_x: {well_length_x}")
-
-    print(f"row_start: {row_start}")
-    # Calculate well locations:
-    # Index starts at 1, because these are physical well locations.
-    for col in range(num_col):
-        # print(col)
-        # x_start = row_start[X]
-        # y_start = row_start[Y]
-        # z_start = row_start[Z]
-        # x_loc = round(x_start + (x_step * col), 2)
-        # y_loc = round(y_start + (y_step * col), 2)
-        # z_loc = round(z_start + (z_step * col), 2)
-        # print(f"col: {col}, x_loc: {x_loc}, y_loc: {y_loc}, z_loc: {z_loc}")
-
-        # Loop Version, go through each key, calculate location
-        well_loc = {}
-        for key in row_start.keys():
-            # print(key)
-            start = row_start[key]
-            step = delta_dict[key] / num_col_jump
-            loc = round(start + (step * col), 2)
-            well_loc[key] = loc
-
-        print(f"col: {col+1}, {well_loc}")
-
-
-    print(f"row_end: {row_end}")
-    # Output those locations
-
-
+    well_loc_list = get_row_locations(row_start, row_end, num_col)
+    print(f"well_loc_list: {well_loc_list}")
 
     pass
 
