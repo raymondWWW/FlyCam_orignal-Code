d0d7f8a66cfc2eddcceba4d135305972046c1deb
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-06-28 01:35:13 -0700

    Update gpiozero_test.py
    Added some more test code, but realized there is no custom device class for the color sensor.

--- testing/Color Sensor/gpiozero_test.py
+++ testing/Color Sensor/gpiozero_test.py
@@ -14,15 +14,60 @@ Color Sensor info
 https://www.best-microcontroller-projects.com/tcs230.html
 """
 
-from gpiozero import Device, LED
-from gpiozero.pins.mock import MockFactory
+from gpiozero import Device, LED, Button
+from gpiozero.pins.mock import MockFactory, MockPWMPin
 
-Device.pin_factory = MockFactory()
 
-led = LED(2)
+def main3():
+    Device.pin_factory = MockFactory(pin_class=MockPWMPin)
 
-print(led.value)
+    led = LED(2)
 
-led.on()
+    print(led.value)
+    pass
 
-print(led.value)
+def main2():
+    Device.pin_factory = MockFactory()
+
+    # LED/Button Test
+    led = LED(17)
+    btn = Button(16)
+
+    # Bug for me, led.source = btn isn't working
+
+    btn.when_pressed = led.on
+    btn.when_released = led.off
+
+    # Initially, the button isn't pressed so the LED should be off
+    print(led.value)
+
+    # Press the button
+    btn.pin.drive_low()
+
+    print(led.value)
+
+    # Release the button
+    btn.pin.drive_high()
+
+    print(led.value)
+
+
+def main():
+    # https://gpiozero.readthedocs.io/en/stable/api_pins.html?highlight=mock#mock-pins
+    # Simple LED off/on check
+    Device.pin_factory = MockFactory()
+
+    led = LED(2)
+
+    print(led.value)
+
+    led.on()
+
+    print(led.value)
+
+
+if __name__ == "__main__":
+    # main()
+    # main2()
+    main3()
+
