1e56a84fd3fa517229d834c3f69ff8a32a5bae51
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-06-27 21:51:28 -0700

    Update map_function_test.py
    Function appears to work.

--- testing/Color Sensor/map_function_test.py
+++ testing/Color Sensor/map_function_test.py
@@ -1,11 +1,29 @@
 """
 Code to mimic Arduino's Map function
 
+Maps one range onto another. It's basically ratios.
 
+Sources:
+https://www.arduino.cc/reference/en/language/functions/math/map/
+*Math formula at the end
 """
 
 
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
 def main():
+
+    x = 11
+    in_min = 10
+    in_max = 20
+    out_min = 0
+    out_max = 100
+
+    x_out = map_range(x, in_min, in_max, out_min, out_max)
+
+    print(f"x_out: {x_out}")
     pass
 
 
