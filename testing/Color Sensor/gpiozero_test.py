ba6657c88bff275dc2742c1551c547d004ff12f3
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-06-27 22:15:17 -0700

    Update gpiozero_test.py
    Added in comments for ideas.

--- testing/Color Sensor/gpiozero_test.py
+++ testing/Color Sensor/gpiozero_test.py
@@ -4,6 +4,14 @@ Code to practice playing around with GPIO Zero
 Sources:
 https://gpiozero.readthedocs.io/en/stable/api_pins.html?highlight=mock#mock-pins
 
+First, test case, replicate code here, but with gpiozero
+https://www.electronicshub.org/raspberry-pi-color-sensor-tutorial/
+
+Color Sensor Datasheet, for mock values?
+https://www.best-microcontroller-projects.com/support-files/tcs230.pdf
+
+Color Sensor info
+https://www.best-microcontroller-projects.com/tcs230.html
 """
 
 from gpiozero import Device, LED
