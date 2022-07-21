c0882753fe6d5341f1421ddad8c3b944e005352c
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-07-20 22:27:37 -0700

    Update module_experiment_timer.py
    Added in demo code to simulate experiment (sleep version)

--- module_experiment_timer.py
+++ module_experiment_timer.py
@@ -4,17 +4,18 @@ Module for setting how long to run experiment
 TODO:
 -Create sample loop like Arduino?
 -Create sample loop using time.sleep?
+- Display time left until next run and time left until experiment is done.
 
 Changelog:
 7-19-2022: Added in functions to create layout, check for digits, and to collect time values.
 """
 
 import PySimpleGUI as sg
+import time
 
-
-DEFAULT_TOTAL_HOURS = "1"
-DEFAULT_TOTAL_MIN = "10"
-DEFAULT_RUN_MIN = "5"
+DEFAULT_TOTAL_HOURS = "0"
+DEFAULT_TOTAL_MIN = "3"
+DEFAULT_RUN_MIN = "1"
 
 TOTAL_HOURS_KEY = "-HOURS-"
 TOTAL_MIN_KEY = "-MIN-"
@@ -59,7 +60,83 @@ def get_hour_min(event, values, window):
     print(f"or experiment will run for {total_seconds} seconds")
 
     run_minutes = int(values[RUN_MIN_KEY])
-    print(f"After collecting data from wells, will wait {run_minutes} minutes before collecting data again")
+    run_seconds = run_minutes * 60
+    print(f"After collecting data from wells, will wait {run_minutes} (or {run_seconds} seconds) minutes before collecting data again")
+
+    # Dummy values for fasting code testing
+    total_seconds = 16
+    run_seconds = 5
+    demo_start_experiment_1(total_seconds, run_seconds)
+
+
+def demo_start_experiment_1(total_seconds, run_seconds):
+    print("demo_start_experiment_1")
+    # Practice function to practice
+    # While loop using time.sleep to pause the script
+
+    # TODO: Display time left until end of experiment and time left until next run.
+
+    # demo location list init
+    location_list = [1, 2, 3, 4]
+
+    # Init start time variable
+    start = time.monotonic()
+
+    # Init location index to zero
+    location_index = 0
+
+    elapsed_seconds = -1
+
+    counter = 0
+
+    # While elapsed time is less than total time
+    while elapsed_seconds < total_seconds:
+        #  Go to location (print it out)
+        # print(location_list[location_index])
+        # time.sleep(1)
+        # location_index += 1
+
+        print(f"Run #{counter}")
+        for loc in location_list:
+            print(loc)
+            time.sleep(1)
+
+        #  Get current time
+        current_time = time.monotonic()
+
+        #  Calculate elapsed time (current - start)
+        elapsed_seconds = current_time - start
+
+        print(f"elapsed_seconds: {elapsed_seconds:.2f}")
+
+        # if location_index >= len(location_list):
+        #     location_index = 0
+            # break  # temp
+
+        #  time.sleep and wait run_seconds
+
+        # Note: Caused GUI to pause and you can't interact with it!
+        #       May need threading, which is what my GUI does.
+
+        # TODO: Figure out how to avoid putting this if statement here.
+        if elapsed_seconds < total_seconds:
+            print(f"Will wait {run_seconds} seconds until collecting data again")
+            time.sleep(run_seconds)
+
+        counter += 1
+
+    print("Done running experiment")
+
+
+    #  iterate location index by one or reset to zero if hit end of list.
+
+
+    pass
+
+
+def demo_start_experiment_2():
+    # While loop using time.monotonic to activate certain conditions the script
+    pass
 
 
 def main():
