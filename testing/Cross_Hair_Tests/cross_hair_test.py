24a7ed92e22c0f9f32bc602df93c4f4b56b6d497
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-08-14 13:25:42 -0700

    Create cross_hair_test.py
    Init Cross Hair Test, Pseudocode created.

---
+++ testing/Cross_Hair_Tests/cross_hair_test.py
@@ -0,0 +1,29 @@
+"""
+Cross Hair Test
+
+Algorithm:
+GUI, button to Load Image (takes from preview)
+GUI, same circle radius as SimpleGUI
+Get preview dimensions
+Load image
+Make a copy of image
+Get dimensions of image
+Get center x, center y.
+On Copy, Draw line, center x, from min y to max y
+On Copy, Draw line, center y, from min x to max x
+On copy, Draw circle at center x/y with radius
+Display Image
+
+"""
+
+
+def main():
+    print("Main")
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
