5ae5a6cb1a99d68cfbea5ad59564045f03712389
Johnny Duong <10577482+JohnDoRight@users.noreply.github.com>
2022-08-14 23:47:42 -0700

    Update cross_hair_test.py
    Can draw lines and circle on image.

--- testing/Cross_Hair_Tests/cross_hair_test.py
+++ testing/Cross_Hair_Tests/cross_hair_test.py
@@ -19,18 +19,21 @@ TODO:
 -Circle overlay opacity? Color change?
 -Research ellipse?
 """
+import cv2
 import PySimpleGUI as sg
+
+from os.path import join
 
 # Events
 LOAD_IMAGE = "Update/Load Image"
 
 # Circle Mask
-CIRCLE_XC = 1350
-CIRCLE_YC = 960
-CIRCLE_CENTER = (CIRCLE_XC, CIRCLE_YC)
-CIRCLE_RADIUS = 760
+# CIRCLE_XC = 1350
+# CIRCLE_YC = 960
+# CIRCLE_CENTER = (CIRCLE_XC, CIRCLE_YC)
+CIRCLE_RADIUS = 100
 CIRCLE_COLOR = (0, 0, 255)
-CIRCLE_THICKNESS = 3
+CIRCLE_THICKNESS = 1
 
 RAD_KEY = "-RADIUS-"
 RAD_MINUS_TEN = "-RAD MINUS_TEN-"
@@ -40,7 +43,7 @@ RAD_PLUS_TEN = "-RAD PLUS TEN-"
 
 
 def update_circle(event, values, window):
-    global CIRCLE_XC, CIRCLE_YC, CIRCLE_RADIUS, CIRCLE_CENTER
+    global CIRCLE_RADIUS
 
     if event == LOAD_IMAGE:
         CIRCLE_RADIUS = int(values[RAD_KEY])
@@ -54,18 +57,68 @@ def update_circle(event, values, window):
     elif event == RAD_PLUS_TEN:
         CIRCLE_RADIUS += 10
 
-    CIRCLE_CENTER = (CIRCLE_XC, CIRCLE_YC)
+    # CIRCLE_CENTER = (CIRCLE_XC, CIRCLE_YC)
     window[RAD_KEY].update(CIRCLE_RADIUS)
+    draw_on_image()
+
+
+def draw_on_image():
+    # Temp image get
+    image = get_dummy_image()
+    image_edit = image.copy()
+
+    # Make a copy of image
+    # Get dimensions of image
+    # print(image.shape)
+    height, width, ch = image.shape
+    # Get center x, center y.
+    center_x = int(width / 2)
+    center_y = int(height / 2)
+    print(f"Center: {center_x, center_y}")
+
+    # On Copy, Draw line, center y, from min x to max x
+    start_point = (0, center_y)
+    end_point = (width, center_y)
+    color = (0, 0, 255) # BGR
+    thickness = 1
+    image_edit = cv2.line(image_edit, start_point, end_point, color, thickness)
+
+    # On Copy, Draw line, center x, from min y to max y
+    start_point = (center_x, 0)
+    end_point = (center_x, height)
+    image_edit = cv2.line(image_edit, start_point, end_point, color, thickness)
+
+    # On copy, Draw circle at center x/y with radius
+    center_coordinates = (center_x, center_y)
+    image_edit = cv2.circle(image_edit, center_coordinates, CIRCLE_RADIUS, color, CIRCLE_THICKNESS)
+
+    # Display Image
+    cv2.imshow("image_edit", image_edit)
+    pass
+
+
+def get_dummy_image():
+    # Dummy Image as placecholder
+    # folder: D:\Documents\SF State\Dr. E Lab\Spring 2022\RoboCam\7-25-2022\Code_Pictures_2022-07-25_171105_auto
+    image_folder = r'D:\Documents\SF State\Dr. E Lab\Spring 2022\RoboCam\7-25-2022\Code_Pictures_2022-07-25_171105_auto'
+    # filename: well1_2022-07-25_171110.jpg
+    image_file = r'well1_2022-07-25_171110.jpg'
+    # image_location
+    image_path = join(image_folder, image_file)
+    # Dummy resize to 640, 480
+    image = cv2.imread(image_path)
+    image_resize = cv2.resize(image, (640, 480))
+
+    return image_resize
 
 
 def main():
     print("Main")
 
-    # Dummy Image as placecholder
-    # folder: D:\Documents\SF State\Dr. E Lab\Spring 2022\RoboCam\7-25-2022\Code_Pictures_2022-07-25_171105_auto
-    # filename: well1_2022-07-25_171110.jpg
-    # image_location
-    # Dummy resize to 640, 480
+
+
+    # cv2.imshow("test", image_resize)
+    # cv2.waitKey(10000)
 
     circle_event_list = [RAD_MINUS_TEN, RAD_MINUS_ONE, RAD_PLUS_ONE, RAD_PLUS_TEN]
 
@@ -96,6 +149,7 @@ def main():
             break
         elif event == LOAD_IMAGE:
             # print(f"Pressed: {LOAD_IMAGE}")
+            draw_on_image()
             update_circle(event, values, window)
 
 
